17532c70bb9fad177ebe3715cfdbe2f51d19fa04
*** failed to import extension hgsubversion from E:/Work/Work/Code/Hg/hgsubversion: [Errno 2] No such file or directory: 'E:/Work/Work/Code/Hg/hgsubversion' <*** failed to import extension hgsubversion from E:/Work/Work/Code/Hg/hgsubversion: [Errno 2] No such file or directory: 'E:/Work/Work/Code/Hg/hgsubversion'@localhost>
2014-07-07 14:50:08 +0800

    ??

--- src/com/github/image/picker/MainActivity.java
+++ src/com/github/image/picker/MainActivity.java
@@ -1,13 +1,22 @@
 package com.github.image.picker;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
 import com.appunity.image.picker.R;
+import com.github.image.picker.util.FileSize;
+import com.github.image.picker.util.ImageZip;
+import com.github.image.picker.util.Utils;
 
 public class MainActivity extends Activity {
     private static final boolean DEBUG = true;
@@ -31,9 +40,21 @@ public class MainActivity extends Activity {
         if (DEBUG) Log.v(TAG, "onActivityResult");
         super.onActivityResult(requestCode, resultCode, data);
         if (DEBUG)
-            Log.v(TAG, "onActivityResult(requestCode" + requestCode
-                    + ", resultCode" + resultCode + ", data" + data + ")");
+            Log.v(TAG, "onActivityResult(requestCode" + requestCode + ", resultCode" + resultCode
+                    + ", data" + data + ")");
         String[] arrayExtra = data.getStringArrayExtra(AlbumListActivity.EXTRA_PICKED_PICTURES);
-        if (DEBUG) Log.v(TAG, Arrays.toString(arrayExtra));
+        Toast.makeText(this, "已选择了" + (arrayExtra != null ? arrayExtra.length : 0) + "张图片",
+                Toast.LENGTH_LONG).show();
+        File cacheDir = Utils.getDiskCacheDir(this, "image_upload");
+        for (String path : arrayExtra) {
+            File file = new File(path);
+            try {
+                Log.v(TAG, path + ", size=" + FileSize.setFile(file).getSize());
+                String zipImage = ImageZip.zipImage(path, cacheDir);
+                Log.v(TAG, zipImage + ", size=" + FileSize.setFile(zipImage).getSize());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
